78a9b94fe9f8b7ded41900102e9bfd70f39c81b4
Ville Lyytikäinen <corpsefin@gmail.com>
2021-06-16 17:07:33 +0300

    Delete youtube.js

--- public/js/youtube.js
+++
@@ -1,6 +0,0 @@
-const googleApiKey = 'AIzaSyDAwNhIq009fO09HMtqmwVeqsCGSIT8wUM';
-const googleAuth = '895151219355-5dkn4n808b0roa1tlrgn3dn4lvsj3d4o.apps.googleusercontent.com';
-
-export async function searchVideo(){
-    
-}
